e80b5d0fab5c9b634e3c1d6eef1c669122def8a1
Vasyl Boyko <vboiko0@gmail.com>
2020-10-24 22:52:53 +0300

    fix: move render logic to root index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,52 @@
-import laba1 from "./labas/laba1";
+import laba1 from "./labas/laba1/index";
 import "./style.css";
 
-laba1.render();
+const renderBlock = {
+  app: document.getElementById("app"),
+
+  addTitle(block) {
+    const { title } = block;
+    if (title) {
+      const el = document.createElement("h2");
+      el.classList.add("page-break-before");
+      el.innerText = title;
+      this.app.appendChild(el);
+    }
+  },
+
+  render(block) {
+    this.addTitle(block);
+    try {
+      this[block.type](block.content);
+    } catch (err) {
+      throw new Error(`Wrong type of block: ${block.type}`);
+    }
+  },
+
+  flowchart(content) {
+    const diagram = flowchart.parse(content);
+    const el = document.createElement("div");
+    el.classList.add("flowchart");
+    this.app.appendChild(el);
+    diagram.drawSVG(el);
+  },
+
+  markdown(content) {
+    const el = document.createElement("div");
+    el.innerHTML = content;
+    this.app.appendChild(el);
+  },
+
+  code(content) {
+    const el = document.createElement("div");
+    const escapedCode = content.replace(/\</g, "&lt;").replace(/\>/g, "&gt;");
+    el.innerHTML = `<pre>${escapedCode}</pre>`;
+    this.app.appendChild(el);
+  },
+};
+
+const render = (blocks) => {
+  blocks.forEach((block) => renderBlock.render(block));
+};
+
+render(laba1.blocks);
--- src/labas/laba1/index.js
+++ src/labas/laba1/index.js
@@ -3,16 +3,24 @@ import text from "./text.md";
 import code from "./laba1.c";
 import "./formula.png";
 
-export default {
-  render() {
-    const diagram = flowchart.parse(chart);
-    diagram.drawSVG("diagram");
-
-    const textEl = document.getElementById("text");
-    textEl.innerHTML = text;
-
-    const codeEl = document.getElementById("code");
-    const escapedCode = code.replace(/\</g, "&lt;").replace(/\>/g, "&gt;");
-    codeEl.innerHTML = `<pre>${escapedCode}</pre>`;
-  },
+const laba1 = {
+  blocks: [
+    {
+      title: "",
+      type: "markdown",
+      content: text,
+    },
+    {
+      title: "Блок-схема алгоритму",
+      type: "flowchart",
+      content: chart,
+    },
+    {
+      title: "Вихідний код програми",
+      type: "code",
+      content: code,
+    },
+  ],
 };
+
+export default laba1;
