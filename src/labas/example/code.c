211f2ea7df130e8efe88ec5bdb4414cfbb49488f
Vasyl Boyko <vboiko0@gmail.com>
2020-10-24 13:25:13 +0300

    add: some text to md file

--- src/labas/example/code.c
+++ src/labas/example/code.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("hello world!\n");
-    return (0);
+    // put your code here
+    double x = 17.32, y, z = 1;
+
+    // int a = 1 b = 2 c = 3;
+
+    // char sym = '2'
+
+    // int b = 1
+
+    char sym = '2';
+
+    int a, b = 0;
+
+    // double x = 17, 32;
+    return 0;
 }
