3e36c4872afba4b216d8418361d70081a0f9f6b0
Vasyl Boyko <vboiko0@gmail.com>
2020-10-29 20:33:34 +0200

    add: clear plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const docsPath = path.resolve(__dirname, "src/docs/");
 
@@ -74,6 +75,7 @@ module.exports = {
     path: path.resolve(__dirname, "docs"),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
       PAGES: JSON.stringify(htmlPageNames),
     }),
