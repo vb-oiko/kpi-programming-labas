0cca7b1ae43715cf5f6d2123c15e3ab8b7d7b663
Vasyl Boyko <vboiko0@gmail.com>
2020-11-04 19:20:43 +0200

    add: title sheet to programming labas

--- src/docs/programming/laba1/index.js
+++ src/docs/programming/laba1/index.js
@@ -1,10 +1,19 @@
 import chart from "./flowchart.flow";
 import text from "./text.md";
-import code from "./laba1.c";
+import code from "./code.c";
+import titleSheet from "@/common/programming-laba-title-sheet.html";
 import { render } from "@/core";
 import "@/style.css";
 
-const laba1 = [
+const blocks = [
+  {
+    title: "",
+    type: "html",
+    content: titleSheet,
+    vars: {
+      labaNo: 1,
+    },
+  },
   {
     title: "",
     type: "markdown",
@@ -22,4 +31,4 @@ const laba1 = [
   },
 ];
 
-render(laba1);
+render(blocks);
--- src/docs/programming/laba3/index.js
+++ src/docs/programming/laba3/index.js
@@ -1,10 +1,19 @@
 import chart from "./flowchart.flow";
 import text from "./text.md";
 import code from "./code.c";
+import titleSheet from "@/common/programming-laba-title-sheet.html";
 import { render } from "@/core";
 import "@/style.css";
 
 const blocks = [
+  {
+    title: "",
+    type: "html",
+    content: titleSheet,
+    vars: {
+      labaNo: 3,
+    },
+  },
   {
     title: "",
     type: "markdown",
