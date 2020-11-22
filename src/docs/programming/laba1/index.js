eb6b754ac7887c13c254d5cd4fbad7dc9d89d747
Vasyl Boyko <vboiko0@gmail.com>
2020-11-22 21:12:08 +0200

    add: screenshot

--- src/docs/programming/laba1/index.js
+++ src/docs/programming/laba1/index.js
@@ -1,5 +1,6 @@
 import chart from "./flowchart.flow";
 import text from "./text.md";
+import text1 from "./text1.md";
 import code from "./code.c";
 import titleSheet from "@/common/programming-laba-title-sheet.html";
 import { render } from "@/core";
@@ -28,6 +29,11 @@ const blocks = [
     title: "Вихідний код програми",
     type: "code",
     content: code,
+  },
+  {
+    title: "Скріншоти виконання програми",
+    type: "markdown",
+    content: text1,
   },
 ];
 
