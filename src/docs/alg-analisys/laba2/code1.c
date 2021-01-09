cd0c6f5aa86c2ee66f006383c767c9c1f5098a9e
Vasyl Boyko <vboiko0@gmail.com>
2021-01-09 21:49:45 +0200

    add: split str to arr

--- src/docs/alg-analisys/laba2/code1.c
+++ src/docs/alg-analisys/laba2/code1.c
@@ -28,17 +28,20 @@ const char caseName[TEST_CASES_NUM][7] = {
 void getWord(char *str, long i);
 char **newStringArray(size_t len);
 char *newString(size_t length);
-char *getTestString(size_t len, size_t *wordCount);
+char *getTestString(size_t len);
+char **splitStrToArray(char *string);
+size_t getWordCount(size_t len);
 
 int verbose = 0;
 
 int main(void)
 {
     srand(times(NULL));
-    size_t wordCount = 0;
-    char *s = getTestString(100, &wordCount);
+    char *s = getTestString(100);
+    size_t wordCount = getWordCount(strlen(s));
     printf("%s\n%zu\n", s, wordCount);
 
+    free(s);
     return 0;
 }
 
@@ -71,17 +74,17 @@ char *newString(size_t length)
     return string;
 }
 
-char *getTestString(size_t len, size_t *wordCount)
+char *getTestString(size_t len)
 {
     char *str = newString(len + 1);
 
     const char *SPACE = " ";
 
-    *wordCount = (len - 2) / (WORD_LEN + 1);
+    size_t wordCount = getWordCount(len);
     size_t pos = 0;
     char word[WORD_LEN + 1];
 
-    for (size_t i = 0; i < *wordCount; i++)
+    for (size_t i = 0; i < wordCount; i++)
     {
         getWord(word, (long)i);
         memcpy(str + pos, word, WORD_LEN);
@@ -91,4 +94,47 @@ char *getTestString(size_t len, size_t *wordCount)
     }
 
     return str;
+}
+
+char **splitStrToArray(char *string)
+{
+    char *pattern = "\\w+";
+    regex_t regex;
+    size_t len = strlen(string);
+    size_t wordCount = getWordCount(len);
+
+    char **arr = newStringArray(wordCount);
+
+    if (regcomp(&regex, pattern, REG_EXTENDED))
+    {
+        printf("Failed to compile regex pattern\n");
+        exit(EXIT_FAILURE);
+    }
+
+    regmatch_t pmatch[1];
+    char *st;
+    char *s = string;
+
+    for (int i = 0;; i++)
+    {
+        if (regexec(&regex, s, ARRAY_SIZE(pmatch), pmatch, 0))
+            break;
+
+        st = s + pmatch[0].rm_so;
+        len = pmatch[0].rm_eo - pmatch[0].rm_so;
+
+        char *word = malloc((len + 1) * sizeof(char));
+        memcpy(word, st, len);
+        word[len + 1] = '\0';
+        arr[i] = word;
+        int eo = pmatch[0].rm_eo;
+        s += (eo + 1);
+    }
+
+    return arr;
+}
+
+size_t getWordCount(size_t len)
+{
+    return (len - 2) / (WORD_LEN + 1);
 }
