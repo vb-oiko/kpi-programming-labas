91e583bb6fc545b38f2579e4555a46315df4cab9
Vasyl Boyko <vboiko0@gmail.com>
2021-11-16 22:25:03 +0200

    split do into separate files

--- src/docs/course-2/semester-2/do/control1/index.js
+++ src/docs/course-2/semester-2/do/control1/index.js
@@ -1,4 +1,9 @@
-import text from "./text.md";
+import task1 from "./task1.md";
+import task2 from "./task2.md";
+import task3 from "./task3.md";
+import task4 from "./task4.md";
+import task5 from "./task5.md";
+
 import { render } from "@/core";
 import titleSheet from "@/common/laba-title-sheet.html";
 import "@/style.css";
@@ -18,7 +23,27 @@ const blocks = [
   {
     title: "",
     type: "markdown",
-    content: text,
+    content: task1,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task2,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task3,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task4,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task5,
   },
 ];
 
