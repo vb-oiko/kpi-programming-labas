618c99cb6948fdcfe4c465cc697084621ae07c2e
Vasyl Boyko <vboiko0@gmail.com>
2021-11-27 12:53:13 +0200

    add do var 1

---
+++ src/docs/course-2/semester-2/do/control1-1/index.js
@@ -0,0 +1,50 @@
+import task1 from "./task1.md";
+import task2 from "./task2.md";
+import task3 from "./task3.md";
+import task4 from "./task4.md";
+import task5 from "./task5.md";
+
+import { render } from "@/core";
+import titleSheet from "@/common/laba-title-sheet-var-1.html";
+import "@/style.css";
+
+const blocks = [
+  {
+    title: "",
+    type: "html",
+    content: titleSheet,
+    vars: {
+      labaNo: 1,
+      professor: "викладач Зелікман А.В.",
+      course: "Дослідження операцій",
+      title: "модульної контрольної",
+    },
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task1,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task2,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task3,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task4,
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task5,
+  },
+];
+
+render(blocks);
