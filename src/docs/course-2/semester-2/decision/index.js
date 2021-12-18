314319f42f34de61df54c7324b254165891111ed
Vasyl Boyko <vboiko0@gmail.com>
2021-12-18 15:16:03 +0200

    add decision 1-1

---
+++ src/docs/course-2/semester-2/decision/index.js
@@ -0,0 +1,25 @@
+import task_1_1 from "./task-1-1.md";
+import { render } from "@/core";
+import titleSheet from "@/common/laba-title-sheet.html";
+import "@/style.css";
+
+const blocks = [
+  {
+    title: "",
+    type: "html",
+    content: titleSheet,
+    vars: {
+      labaNo: 4,
+      professor: "проф. Зайченко Ю.П.",
+      course: "Теорія прийняття рішень",
+      title: "контрольної",
+    },
+  },
+  {
+    title: "",
+    type: "markdown",
+    content: task_1_1,
+  },
+];
+
+render(blocks);
