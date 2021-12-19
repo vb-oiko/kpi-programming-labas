e9db7c0a1e2290ee96844eb7981b5f4e844180b6
Vasyl Boyko <vboiko0@gmail.com>
2021-12-19 19:54:40 +0200

    fix bug, add task-2

--- src/docs/course-2/semester-2/decision/task-2/index.js
+++ src/docs/course-2/semester-2/decision/task-2/index.js
@@ -1,25 +1,9 @@
-import task_1_1 from "./task-1-1/text.md";
-import task_1_2 from "./task-1-2/text.md";
-import task_1_3 from "./task-1-3/text.md";
-import task_1_4 from "./task-1-4/text.md";
-import task_1_5 from "./task-1-5/text.md";
-import task_1_6 from "./task-1-6/text.md";
-import task_1_7 from "./task-1-7/text.md";
-import task_1_8 from "./task-1-8/text.md";
+import task_1 from "./task1.md";
 import { render } from "@/core";
 import titleSheet from "@/common/laba-title-sheet-var-1.html";
 import "@/style.css";
 
-const contents = [
-  task_1_1,
-  task_1_2,
-  task_1_3,
-  task_1_4,
-  task_1_5,
-  task_1_6,
-  task_1_7,
-  task_1_8,
-];
+const contents = [task_1];
 
 const blocks = [
   {
@@ -27,7 +11,7 @@ const blocks = [
     type: "html",
     content: titleSheet,
     vars: {
-      labaNo: 1,
+      labaNo: 2,
       professor: "проф. Зайченко Ю.П.",
       course: "Теорія прийняття рішень",
       title: "контрольної",
