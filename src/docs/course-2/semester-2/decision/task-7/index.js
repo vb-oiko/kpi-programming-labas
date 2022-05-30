f380b8947cca9d10590274a465f3e0b285d24729
Vasyl Boyko <vboiko0@gmail.com>
2022-05-30 20:29:06 +0300

    add task 7-4

--- src/docs/course-2/semester-2/decision/task-7/index.js
+++ src/docs/course-2/semester-2/decision/task-7/index.js
@@ -1,15 +1,16 @@
 import task1 from "./task-1/task1.md";
 import task2 from "./task-2/task2.md";
 import task3 from "./task-3/task3.md";
+import task4 from "./task-4/task4.md";
 import { render } from "@/core";
 import titleSheet from "@/common/tpr-title-sheet.html";
 import "@/style.css";
 
 const contents = [
-  task1,
-  task2,
-  task3,
-  // task4,
+  // task1,
+  // task2,
+  // task3,
+  task4,
   // task5,
   // task6,
   // task7,
