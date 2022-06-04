cbe9f76ee3461b210a52e410c9711a173a1558be
Vasyl Boyko <vboiko0@gmail.com>
2022-06-04 13:30:45 +0300

    add task 8-6

--- src/docs/course-2/semester-2/decision/task-8/index.js
+++ src/docs/course-2/semester-2/decision/task-8/index.js
@@ -13,6 +13,12 @@ import task4_3 from "./task4-3.md";
 import task5_1 from "./task5-1.md";
 import task5_2 from "./task5-2.md";
 import task5_3 from "./task5-3.md";
+import task6_1 from "./task6-1.md";
+import task6_2 from "./task6-2.md";
+import task6_3 from "./task6-3.md";
+// import task7_1 from "./task7-1.md";
+// import task7_2 from "./task7-2.md";
+// import task7_3 from "./task7-3.md";
 import { render } from "@/core";
 import titleSheet from "@/common/tpr-title-sheet.html";
 import "@/style.css";
@@ -31,9 +37,15 @@ const contents = [
   // task4_1,
   // task4_2,
   // task4_3,
-  task5_1,
-  task5_2,
-  task5_3,
+  // task5_1,
+  // task5_2,
+  // task5_3,
+  // task6_1,
+  // task6_2,
+  task6_3,
+  // task7_1,
+  // task7_2,
+  // task7_3,
 ];
 
 const blocks = [
