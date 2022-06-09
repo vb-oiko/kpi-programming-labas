8c3ad667b083c6af4dbd73383c414e91616208fc
Vasyl Boyko <vboiko0@gmail.com>
2022-06-09 23:10:27 +0300

    add smai laba 1-1

--- src/docs/course-2/semester-2/smai/task1/index.js
+++ src/docs/course-2/semester-2/smai/task1/index.js
@@ -1,29 +1,27 @@
 import a_star from "./a_star.mmd";
+import greedy from "./greedy.mmd";
 import text from "./text.md";
-// import titleSheet from "@/common/programming-laba-title-sheet.html";
 import { render } from "@/core";
 import "@/style.css";
 
 const blocks = [
-  // {
-  //   title: "",
-  //   type: "html",
-  //   content: titleSheet,
-  //   vars: {
-  //     labaNo: 1,
-  //   },
-  // },
   {
     title: "",
     type: "markdown",
     content: text,
   },
   {
-    title: "Блок-схема алгоритму",
     type: "mermaid",
     content: a_star,
     vars: {
       id: "a_star",
+    },
+  },
+  {
+    type: "mermaid",
+    content: greedy,
+    vars: {
+      id: "greedy",
     },
   },
 ];
